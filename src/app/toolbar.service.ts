85adf67dd17d510f642a24c601f26621195a0cce
jordidh <jordi.dh@gmail.com>
2020-05-07 10:37:49 +0200

    2 files modified - neteja de l'àrea de treball d'etiquetes

--- src/app/toolbar.service.ts
+++ src/app/toolbar.service.ts
@@ -8,26 +8,33 @@ import { HttpClient } from '@angular/common/http';
 export class ToolbarService {
   // Eines que l'usuari actual pot fer servir en cada pantalla
   tools = [ 
-    { menuId:1, tools: [ 
-      { id:1, text:'new', route:'#'}, 
-      { id:2, text:'edit', route:'#'},
-      { id:3, text:'delete', route:'#'},
-      { id:4, text:'print', route:'#'},
-      { id:5, text:'export', route:'#'} 
+    { menuId:10, tools: [ 
+      { id:101, text:'new', route:'#'}, 
+      { id:102, text:'edit', route:'#'},
+      { id:103, text:'delete', route:'#'},
+      { id:104, text:'print', route:'#'},
+      { id:105, text:'export', route:'#'} 
+      ] },
+    { menuId:11, tools: [ 
+      { id:101, text:'new', route:'#'}, 
+      { id:102, text:'edit', route:'#'},
+      { id:103, text:'delete', route:'#'},
+      { id:104, text:'print', route:'#'},
+      { id:105, text:'export', route:'#'} 
       ] },
     { menuId:2, tools: [ 
-      { id:6, text:'new', route:'#'}, 
-      { id:7, text:'edit', route:'#'},
-      { id:8, text:'delete', route:'#'},
-      { id:9, text:'print', route:'#'},
-      { id:10, text:'export', route:'#'} 
+      { id:21, text:'new', route:'#'}, 
+      { id:22, text:'edit', route:'#'},
+      { id:23, text:'delete', route:'#'},
+      { id:24, text:'print', route:'#'},
+      { id:25, text:'export', route:'#'} 
       ] },
     { menuId:3, tools: [ 
-      { id:11, text:'new', route:'#'}, 
-      { id:12, text:'edit', route:'#'},
-      { id:13, text:'delete', route:'#'},
-      { id:14, text:'print', route:'#'},
-      { id:15, text:'export', route:'#'} 
+      { id:31, text:'new', route:'#'}, 
+      { id:32, text:'edit', route:'#'},
+      { id:33, text:'delete', route:'#'},
+      { id:34, text:'print', route:'#'},
+      { id:35, text:'export', route:'#'} 
       ] }
   ];
 
